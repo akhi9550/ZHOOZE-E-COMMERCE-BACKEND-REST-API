ce0601a77466bd3dd340133ccecd746dc64b8b5d
akhi9550 <akhilc248@gmail.com>
2024-03-29 00:53:00 +0530

    updates

--- pkg/domain/offer.go
+++ pkg/domain/offer.go
@@ -12,9 +12,6 @@ type ProductOffer struct {
 	DiscountPercentage int       `json:"discount_percentage"`
 	StartDate          time.Time `json:"start_date"`
 	EndDate            time.Time `json:"end_date"`
-	// OfferLimit         int       `json:"offer_limit"`
-	// OfferUsed          int       `json:"offer_used"`
-	// Valid bool `json:"valid" gorm:"default:True"`
 }
 
 type CategoryOffer struct {
@@ -26,25 +23,3 @@ type CategoryOffer struct {
 	StartDate          time.Time `json:"start_date"`
 	EndDate            time.Time `json:"end_date"`
 }
-
-// type ProductOfferUsed struct {
-// 	gorm.Model
-// 	UserID         uint         `json:"user_id"`
-// 	Users          User         `json:"-" gorm:"foreignkey:UserID"`
-// 	ProductOfferID uint         `json:"product_offer_id"`
-// 	ProductOffer   ProductOffer `json:"-" gorm:"constraint:OnDelete:CASCADE;foreignKey:ProductOfferID"`
-// 	OfferAmount    float64      `json:"offer_amount"`
-// 	OfferCount     int          `json:"offer_count"`
-// 	Used           bool         `json:"used"`
-// }
-
-// type CategoryOfferUsed struct {
-// 	gorm.Model
-// 	UserID          uint          `json:"user_id"`
-// 	Users           User          `json:"-" gorm:"foreignkey:UserID"`
-// 	CategoryOfferID uint          `json:"product_offer_id"`
-// 	CategoryOffer   CategoryOffer `json:"-" gorm:"constraint:OnDelete:CASCADE;foreignKey:CategoryOfferID"`
-// 	OfferAmount     float64       `json:"offer_amount"`
-// 	OfferCount      int           `json:"offer_count"`
-// 	Used            bool          `json:"used"`
-// }
